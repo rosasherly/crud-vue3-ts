56d7b282c16320132510d9a5b03af1085456cfbc
rosasherly <rosasherly979@gmail.com>
2026-09-22 22:21:25 +0700

    chore: update main app layout

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav me-auto mb-4 mb-lg-0">
             <li class="nav-item">
               <router-link
                 to="/products"
@@ -26,14 +26,14 @@
                 >PRODUCTS</router-link
               >
             </li>
-          </ul>
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0" role="search">
-            <a
-              href="https://santrikoding.com"
-              target="_blank"
-              class="btn btn-success"
-              >SANTRIKODING.COM</a
-            >
+            <li class="nav-item">
+              <router-link
+                to="/categories"
+                class="nav-link active"
+                aria-current="page"
+                >CATEGORIES</router-link
+              >
+            </li>
           </ul>
         </div>
       </div>
